7535c45104c2d9d88e8c412eb318ba2f8b73ab64
Leo <cargma114@gmail.com>
2026-06-26 20:35:57 -0300

    login funcionando ok ta bom

--- script/script.js
+++ script/script.js
@@ -1,3 +1,8 @@
+// usuário "cadastrado" fixo
+const usuarios = [
+    { email: "infinitypromo@gmail.com", senha: "1234" }
+];
+
 document.getElementById("btnEntrar").onclick = () => {
     const email = document.getElementById("email").value;
     const senha = document.getElementById("password").value;
@@ -14,6 +19,13 @@ document.getElementById("btnEntrar").onclick = () => {
     }
     if (!senha) {
         senhaError.innerText = "Digite sua senha";
+        return;
+    }
+
+    const usuarioEncontrado = usuarios.find(u => u.email === email && u.senha === senha);
+
+    if (!usuarioEncontrado) {
+        senhaError.innerText = "Email ou senha incorretos";
         return;
     }
 
